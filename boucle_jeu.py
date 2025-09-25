364fab33696ba8697995dbb4c3b24774e5779341
Hokinai1 <hokinaiesso@gmail.com>
2025-09-25 23:45:48 +0000

    Les conditions en python

---
+++ boucle_jeu.py
@@ -0,0 +1,9 @@
+# EXERCICE JEU DE PRIX
+
+# Choisir un nombre entre 1 et 1000+
+# tant que le jeu n'est pas fini
+#     demander à l'utilisateur d'entrer un prix
+#     s'il trouve le juste prix , c'est gagné .
+#     sinon on affiche c'est mois ou c'est plus.
+
+
---
+++ conditions.py
@@ -0,0 +1,11 @@
+
+# les conditions  
+
+age = 15
+
+sexe = "F"
+
+if sexe == "M" :
+    print(f"Bonjour Monsieur")
+else : 
+    print(f"Bonjour Madame")
